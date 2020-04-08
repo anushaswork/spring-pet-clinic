5baae51365dcfdf7509db080e4a5453445fb6acf
Anusha Srinivasan <anushaswork@gmail.com>
2020-04-08 18:56:17 +0530

    First Commit

---
+++ spring-pet-clilinc/src/main/java/spring/udemy/pet/clinic/springpetclilinc/SpringPetClilincApplication.java
@@ -0,0 +1,13 @@
+package spring.udemy.pet.clinic.springpetclilinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClilincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClilincApplication.class, args);
+	}
+
+}
---
+++ spring-pet-clilinc/src/test/java/spring/udemy/pet/clinic/springpetclilinc/SpringPetClilincApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.udemy.pet.clinic.springpetclilinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClilincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
